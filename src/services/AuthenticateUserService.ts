c29cf8276128bbc1626b0a4e9a86f738f8653c18
marianalopes.dev <marianalopes.dev@gmail.com>
2020-06-01 06:20:10 -0300

    JWT Token

--- src/services/AuthenticateUserService.ts
+++ src/services/AuthenticateUserService.ts
@@ -1,5 +1,6 @@
 import { getRepository } from 'typeorm';
 import { compare } from 'bcryptjs';
+import { sign, verify } from 'jsonwebtoken';
 
 import User from '../models/User';
 
@@ -10,6 +11,7 @@ interface Request {
 
 interface Response {
     user: User;
+    token: string;
 }
 
 class AuthenticateUserService {
@@ -28,8 +30,14 @@ class AuthenticateUserService {
             throw new Error('Incorrect email/password combination.');
         }
 
+        const token = sign({}, '008a466bd0e3352364665f513b9005d6', {
+            subject: user.id,
+            expiresIn: '1d',
+        });
+
         return {
             user,
+            token,
         };
     }
 }
