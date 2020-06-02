2aa9cbe1a1770265dfbf817ae5da059b5fd66bab
marianalopes.dev <marianalopes.dev@gmail.com>
2020-06-02 05:53:43 -0300

    Revert "Authenticated Routes"
    This reverts commit 11b970e18cf966066fd059c8f43af84097cf11b2.

--- src/config/upload.ts
+++ src/config/upload.ts
@@ -3,12 +3,9 @@ import crypto from 'crypto';
 
 import multer from 'multer';
 
-const tmpFolder = path.resolve(__dirname, '..', '..', 'tmp');
 export default {
-    directory: tmpFolder,
-
     storage: multer.diskStorage({
-        destination: tmpFolder,
+        destination: path.resolve(__dirname, '..', '..', 'tmp'),
         filename(request, file, callback) {
             const fileHash = crypto.randomBytes(10).toString('hex');
             const fileName = `${fileHash}-${file.originalname}`;
--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -4,7 +4,6 @@ import uploadConfig from '../config/upload';
 
 import CreateUserService from '../services/CreateUserService';
 import ensureAuthenticated from '../middleware/ensureAuthenticated';
-import UpdateAvatarUserService from '../services/UpdateUserAvatarService';
 
 const usersRouters = Router();
 const upload = multer(uploadConfig);
@@ -35,18 +34,7 @@ usersRouters.patch(
     upload.single('avatar'),
     async (request, response) => {
         // console.log(request.file);
-        try {
-            const updateUserAvatar = new UpdateAvatarUserService();
-            const user = await updateUserAvatar.execute({
-                user_id: request.user.id,
-                avatarFilename: request.file.filename,
-            });
-
-            delete user.password;
-            return response.json(user);
-        } catch (err) {
-            return response.status(400).json({ error: err.message });
-        }
+        return response.json({ ok: true });
     },
 );
 
