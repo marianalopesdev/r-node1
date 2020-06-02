e16359f8e4943773ecaed6a33d2101c05da74d63
marianalopes.dev <marianalopes.dev@gmail.com>
2020-06-02 06:26:55 -0300

    File - static route

--- src/server.ts
+++ src/server.ts
@@ -2,10 +2,11 @@ import 'reflect-metadata';
 
 import express from 'express';
 import routes from './routes';
-import './database';
+import uploadConfig from './config/upload';
 
 const api = express();
 api.use(express.json());
+api.use('/files', express.static(uploadConfig.directory));
 api.use(routes);
 
 api.listen('3334', () => {
